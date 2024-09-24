c71a5644f1b93cd03e53c83a6c97ff04e0f85fb3
PolyStefane <polyvstefane@gmail.com>
2024-09-24 03:15:41 -0300

    test: ensure correct balance 6,6  weights test

--- src/test/java/org/example/BalançaPresentesTeste.java
+++ src/test/java/org/example/BalançaPresentesTeste.java
@@ -8,10 +8,18 @@ import static org.junit.Assert.assertEquals;
 public class BalançaPresentesTeste {
     @Test
     @DisplayName("Should be possible balance the gifts of weights 4,6,2 in the sleigh")
-    public void ShouldBePossibleBalanceTheGiftsOfWeights46Or2(){
+    public void ShouldBePossibleBalanceTheGiftsOfWeights46And2(){
         int[] pesos = {4,6,2};
         assertEquals("S", Balanca.Equilibrou(pesos));
     }
+
+    @Test
+    @DisplayName("Should be possible balance the gifts of weights 6,6 in the sleigh")
+    public void ShouldBePossibleBalanceTheGiftsOfWeights6And6(){
+        int[] pesos = {6,6};
+        assertEquals("N", Balanca.Equilibrou(pesos));
+    }
+
 
 
 
