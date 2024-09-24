cbd3bdd68d7f52cda5d68330ebf2d9c11b9000c0
PolyStefane <polyvstefane@gmail.com>
2024-09-24 03:54:38 -0300

    feat: implements `Balanca` function

--- src/main/java/org/example/Balanca.java
+++ src/main/java/org/example/Balanca.java
@@ -2,6 +2,21 @@ package org.example;
 
 public class Balanca {
     public static String Equilibrou(int[] pesos){
-        return "";
+        int ladoA = 0;
+        int ladoB = 0;
+
+        for (int peso: pesos){
+
+            if((ladoA + peso) - ladoB <= 5){
+                ladoA += peso;
+            } else if ((ladoB + peso) - ladoA <= 5) {
+                ladoB += peso;
+            } else {
+                return("N");
+            }
+        }
+        return "S";
+
+
     }
 }
