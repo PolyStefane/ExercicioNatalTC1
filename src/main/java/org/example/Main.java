d97dc7999f2aad470a97000a756d1293dfe32eb5
PolyStefane <polyvstefane@gmail.com>
2024-09-24 04:12:59 -0300

    feat: add user input for gift weights

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Quantidade de presentes:");
+        int x = scanner.nextInt();
+        int[] pesos = new int[x];
 
+        for (int i = 0; i < x; i++) {
+            System.out.println("Informe o peso numero " + (i+1) + ":");
+            pesos[i] = scanner.nextInt();
+        }
 
+        System.out.println(Balanca.Equilibrou(pesos));
     }
+
 }
